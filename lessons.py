ba3b7de1912d818fafe5492da65cdf8738f73f73
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:50:01 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 7')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #6: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Making an effort leads to long lasting fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • The True Voice encourages us to make an effort and the Opponent Voice challenges us to take the easy way out.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Making an effort earns us long-lasting fulfillment and gives us feelings of empowerment, confidence, ownership and pride. Taking the easy way out only satisfies us temporarily and gives us feelings of self-doubt, shame, unworthiness and fear.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • In order to win the Game of Life, each one of us needs to identify the areas in our lives where we need to put forth effort and overcome challenges.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Success is based on the ability to make an effort to overcome challenges, not necessarily the outcome.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Effort: Working hard to achieve a goal; leads to long lasting fulfillment')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Easy Way Out: Not making an effort; leads to temporary fulfillment')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Think of a time when you put forth effort. What did you do? How did it feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Think of a time when you took the easy way out. What did you do? How did it feel?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 7')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #6: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Making an effort leads to long lasting fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • The True Voice encourages us to make an effort and the Opponent Voice challenges us to take the easy way out.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Making an effort earns us long-lasting fulfillment and gives us feelings of empowerment, confidence, ownership and pride. Taking the easy way out only satisfies us temporarily and gives us feelings of self-doubt, shame, unworthiness and fear.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • In order to win the Game of Life, each one of us needs to identify the areas in our lives where we need to put forth effort and overcome challenges.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Success is based on the ability to make an effort to overcome challenges, not necessarily the outcome.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Effort: Working hard to achieve a goal; leads to long lasting fulfillment')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Easy Way Out: Not making an effort; leads to temporary fulfillment')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Think of a time when you put forth effort. What did you do? How did it feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Think of a time when you took the easy way out. What did you do? How did it feel?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 8 (Part 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #7: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Reactive behaviors lead to temporary fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Challenges trigger reactive feelings; reactive feelings lead to reactive behaviors.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Reactive behaviors give us only temporary fulfillment.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • We have the power to choose to stop our reactive behaviors.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Reactive behaviors are expressed in different ways, and we each need to identify our own reactive behaviors.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • To stop reactive behaviors and achieve long-lasting fulfillment, use the Four-Step Formula.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Reactive feelings: Emotions we experience when faced with a challenge that make the Opponent Voice grow louder')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Reactive behaviors: Actions we choose when we listen to the Opponent Voice')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Four-Step formula: Stop, Ask, Chill Out, Share')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • What happens to your Opponent Voice when you feel reactive?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What can you do to help yourself when you have reactive feelings and hear the Opponent Voice?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 8 (Part 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #7: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Reactive behaviors lead to temporary fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Challenges trigger reactive feelings; reactive feelings lead to reactive behaviors.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Reactive behaviors give us only temporary fulfillment.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • We have the power to choose to stop our reactive behaviors.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Reactive behaviors are expressed in different ways, and we each need to identify our own reactive behaviors.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • To stop reactive behaviors and achieve long-lasting fulfillment, use the Four-Step Formula.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Reactive feelings: Emotions we experience when faced with a challenge that make the Opponent Voice grow louder')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Reactive behaviors: Actions we choose when we listen to the Opponent Voice')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Four-Step formula: Stop, Ask, Chill Out, Share')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • What happens to your Opponent Voice when you feel reactive?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What can you do to help yourself when you have reactive feelings and hear the Opponent Voice?')),
+)
---
+++ videos.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("7.1.ogv",
+     _("The Missing Piece"),
+     _("Rule #6: Making an effort leads to long lasting fulfillment.\n\
+The True Voice encourages us to make an effort and the Opponent Voice challenges us to take the easy way out."),
+      ),
+    ("7.2.ogv",
+     _("Making an Effort"),
+     _("Rule #6: Making an effort leads to long lasting fulfillment.\n\
+The True Voice encourages us to make an effort and the Opponent Voice challenges us to take the easy way out."),
+      ),
+    ("8.1.ogv",
+     _('Using the Formula (Part 1)'),
+     _("Rule #7: Reactive behaviors lead to temporary fulfillment.\n\
+Challenges trigger reactive feelings; reactive feelings lead to reactive behaviors.\n\
+Reactive behaviors give us only temporary fulfillment.\n\
+We have the power to choose to stop our reactive behaviors."),
+     ),
+    ("8.4.ogv",
+     _('Using the Formula (Part 2)'),
+     _("Rule #7: Reactive behaviors lead to temporary fulfillment.\n\
+Challenges trigger reactive feelings; reactive feelings lead to reactive behaviors.\n\
+Reactive behaviors give us only temporary fulfillment.\n\
+We have the power to choose to stop our reactive behaviors."),
+     ),
+)
